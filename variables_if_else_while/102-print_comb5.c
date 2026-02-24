8f58feb5adbc8c126c867f3a6af9a081fd2f2ca2
alaa2026ali <ali_alaa18@icloud.com>
2026-02-24 12:49:07 +0300

    Update 102-print_comb5.c

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -22,7 +22,7 @@ int main(void)
             putchar((j / 10) + '0');
             putchar((j % 10) + '0');
 
-            if (!(i == 98 && j == 99)) /* لا نضع فاصلة بعد آخر رقم */
+            if (!(i == 98 && j == 99)) 
             {
                 putchar(',');
                 putchar(' ');
