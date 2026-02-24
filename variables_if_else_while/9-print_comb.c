68a0fa1d577910ef35c88fd15097c7e036dff356
alaa2026ali <ali_alaa18@icloud.com>
2026-02-24 11:53:41 +0300

    Update 9-print_comb.c

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,26 +1,23 @@
 #include <stdio.h>
 
 /**
- * main - prints single digit numbers separated by comma and space
+ * main - Prints all single digit numbers separated by comma and space
  *
  * Return: 0
  */
 int main(void)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < 10; i++)
-    {
-        putchar(i + '0');   /* print number */
-
-        if (i != 9)         /* if not the last number */
-        {
-            putchar(',');   /* print comma */
-            putchar(' ');   /* print space */
-        }
-    }
-
-    putchar('\n');          /* new line at the end */
-
-    return (0);
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
