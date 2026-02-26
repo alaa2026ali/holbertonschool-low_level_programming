ea01b4ff93086eb02faf693181407a832a49265a
alaa2026ali <ali_alaa18@icloud.com>
2026-02-26 10:51:42 +0300

    Update 100-times_table.c

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,5 +1,30 @@
 #include "main.h"
+/**
+ * print_number - prints a number with proper spacing
+ * @n: number to print
+ * @col: current column
+ */
+void print_number(int n, int col)
+{
+	if (col != 0)
+	{
+		_putchar(',');
+		_putchar(' ');
+		if (n < 10)
+		{
+			_putchar(' ');
+			_putchar(' ');
+		}
+		else if (n < 100)
+			_putchar(' ');
+	}
 
+	if (n >= 100)
+		_putchar((n / 100) + '0');
+	if (n >= 10)
+		_putchar(((n / 10) % 10) + '0');
+	_putchar((n % 10) + '0');
+}
 /**
  * print_times_table - prints the n times table
  * @n: number of times table
@@ -7,45 +32,14 @@
 void print_times_table(int n)
 {
 	int row, col, result;
-
 	if (n < 0 || n > 15)
 		return;
-
 	for (row = 0; row <= n; row++)
 	{
 		for (col = 0; col <= n; col++)
 		{
 			result = row * col;
-
-			if (col == 0)
-			{
-				_putchar(result + '0');
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-
-				if (result < 10)
-				{
-					_putchar(' ');
-					_putchar(' ');
-				}
-				else if (result < 100)
-				{
-					_putchar(' ');
-				}
-
-				if (result >= 100)
-				{
-					_putchar((result / 100) + '0');
-				}
-				if (result >= 10)
-				{
-					_putchar(((result / 10) % 10) + '0');
-				}
-				_putchar((result % 10) + '0');
-			}
+			print_number(result, col);
 		}
 		_putchar('\n');
 	}
