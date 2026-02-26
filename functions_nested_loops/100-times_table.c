787f79422a64d9ddc57e6ae7392524900ed86dcd
alaa2026ali <ali_alaa18@icloud.com>
2026-02-26 10:53:56 +0300

    Fix formatting and spacing in times table functions

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_number - prints a number with proper spacing
  * @n: number to print
@@ -10,6 +11,7 @@ void print_number(int n, int col)
 	{
 		_putchar(',');
 		_putchar(' ');
+
 		if (n < 10)
 		{
 			_putchar(' ');
@@ -25,6 +27,7 @@ void print_number(int n, int col)
 		_putchar(((n / 10) % 10) + '0');
 	_putchar((n % 10) + '0');
 }
+
 /**
  * print_times_table - prints the n times table
  * @n: number of times table
@@ -32,8 +35,10 @@ void print_number(int n, int col)
 void print_times_table(int n)
 {
 	int row, col, result;
+
 	if (n < 0 || n > 15)
 		return;
+
 	for (row = 0; row <= n; row++)
 	{
 		for (col = 0; col <= n; col++)
