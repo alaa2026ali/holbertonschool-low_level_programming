446e2f7921d0cc176c2dc012f00bd05ad6f1d772
alaa2026ali <ali_alaa18@icloud.com>
2026-02-26 08:13:12 +0000

    Add 103-fibonacci.c

---
+++ functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - calculates and prints the sum of even Fibonacci numbers
+ *        not exceeding 4,000,000
+ *
+ * Return: 0
+ */
+int main(void)
+{
+    unsigned long first = 1, second = 2, next;
+    unsigned long sum = 2; /* Start with the first even Fibonacci number */
+
+    while (1)
+    {
+        next = first + second;
+        if (next > 4000000)
+            break;
+        if (next % 2 == 0)
+            sum += next;
+        first = second;
+        second = next;
+    }
+
+    printf("%lu\n", sum);
+    return (0);
+}
