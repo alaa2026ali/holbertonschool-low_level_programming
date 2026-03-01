c014cb303be8736ef7023027f1f249e5d7be3207
alaa2026ali <ali_alaa18@icloud.com>
2026-03-01 12:34:58 +0300

    Update 7-print_diagonal.c

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -7,7 +7,7 @@ int i = 0, j;
 while (i < n)  // loop for each line
 {
  = 0;
-hile (j < i)  // print spaces
+while (j < i)  // print spaces
 {
 _putchar(' ');
 j++;
@@ -19,5 +19,5 @@ i++;
 }
 
 if (n <= 0)  // if n is 0 or negative
-putchar('\n');
+_putchar('\n');
 }
