947fd5893fc4511eb66a1bc91dfcc986cd68b632
alaa2026ali <ali_alaa18@icloud.com>
2026-03-01 12:40:59 +0300

    Update 7-print_diagonal.c

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -8,23 +8,23 @@
  */
 void print_diagonal(int n)
 {
-int i, j; 
+    int i, j;
 
-i = 0;  
+    i = 0;
 
-while (i < n)  
-{
-j = 0;  
- while (j < i)  
-{
-_putchar(' ');
-j++;
-}
-_putchar('\\');  
-_putchar('\n');  
-i++;
-}
+    while (i < n)
+    {
+        j = 0;
+        while (j < i)
+        {
+            _putchar(' ');
+            j++;
+        }
+        _putchar('\\');
+        _putchar('\n');
+        i++;
+    }
 
-if (n <= 0)  
-_putchar('\n');
+    if (n <= 0)
+        _putchar('\n');
 }
