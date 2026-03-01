e1dcf31a48dd96a38de5a4603c9ec4c143102627
alaa2026ali <ali_alaa18@icloud.com>
2026-03-01 11:48:58 +0300

    Update 6-print_line.c

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -8,15 +8,10 @@
  */
 void print_line(int n)
 {
-    int i;
-
-    /* Loop from 0 to n - 1 */
-    for (i = 0; i < n; i++)
-    {
-        if (n > 0)  /* Optional check, the loop won't run if n <= 0 */
-            _putchar('_');
-    }
-
-    /* Print newline at the end */
-    _putchar('\n');
+int i;
+for (i = 0; i < n; i++)
+{
+if (n > 0)  
+_putchar('_\n');
+}
 }
