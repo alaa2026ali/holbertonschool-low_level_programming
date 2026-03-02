ae0f336600a33ac4232a2c1cf053039bcd5910e0
alaa2026ali <ali_alaa18@icloud.com>
2026-03-02 12:59:52 +0300

    Update 4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -6,17 +6,17 @@
  */
 void print_rev(char *s)
 {
-	char *end = s;
+char *end = s;
 
-	while (*end != '\0')
-		end++;
+while (*end != '\0')
+end++;
 
-	end--; 
+end--; 
 
-	while (end >= s)
-	{
-		_putchar(*end);
-		end--;
-	}
-	_putchar('\n');
+while (end >= s)
+{
+_putchar(*end);
+end--;
+}
+_putchar('\n');
 }
