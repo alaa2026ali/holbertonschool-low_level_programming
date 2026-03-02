545ed34a46c5fca56ab45fc589953a3900e9a5e9
alaa2026ali <ali_alaa18@icloud.com>
2026-03-02 11:20:32 +0300

    Update 10-print_triangle.c

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -6,18 +6,20 @@
  */
 void print_triangle(int size)
 {
+	int i, j; 
+
 	if (size <= 0)
 	{
 		_putchar('\n');
 		return;
 	}
 
-	for (int i = 1; i <= size; i++)
+	for (i = 1; i <= size; i++)
 	{
-		for (int j = 1; j <= size - i; j++)
+		for (j = 1; j <= size - i; j++)
 			_putchar(' ');
 
-		for (int j = 1; j <= i; j++)
+		for (j = 1; j <= i; j++)
 			_putchar('#');
 
 		_putchar('\n');
