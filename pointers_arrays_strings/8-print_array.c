ed737590adc3bbe69a1c6841d756cbdf232ec18b
alaa2026ali <ali_alaa18@icloud.com>
2026-03-03 11:24:15 +0300

    Update 8-print_array.c

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -4,16 +4,14 @@
  * print_array - prints n elements of an integer array
  * @a: pointer to the first element
  * @n: number of elements
- *
- * Elements are separated by ", " and ends with \n
- */
+  */
 void print_array(int *a, int n)
 {
 	int i;
 
 	for (i = 0; i < n; i++)
 	{
-		if (i != 0)      
+		if (i != 0)
 			printf(", ");
 		printf("%d", a[i]);
 	}
