1bf309b56c7069ed6e444c6a3111c22d24d875bb
alaa2026ali <ali_alaa18@icloud.com>
2026-03-03 12:08:08 +0300

    Update 100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,43 +1,33 @@
 #include "main.h"
 #include <limits.h>
+
 /**
- * _atoi - Converts a string to an integer, handling signs and non-numeric chars.
- * @s: The input string.
+ * _atoi - converts a string to an integer safely
+ * @s: pointer to string
  *
- * Return: The converted integer. Returns 0 if no numbers found.
+ * Return: integer value of string
  */
 int _atoi(char *s)
 {
-    unsigned int result = 0;
-    int sign = 1;
-    int digit_found = 0;
-    int overflow_limit_div10 = 214748364; 
+	unsigned int result = 0;
+	int sign = 1;
+	int digit_found = 0;
+	int overflow_limit_div10 = 214748364;
 
-    while (*s != '\0')
-    {
-        if (*s == '-')
-        {
-            sign *= -1;
-        }
-        else if (*s == '+')
-        {
-        }
-        else if (*s >= '0' && *s <= '9')
-        {
-            digit_found = 1;
-            if (result > (unsigned int)overflow_limit_div10)
-            {
-          
-            }
-            result = (result * 10) + (*s - '0');
-        }
-        /* If a digit was found and the next char is not a digit, stop processing */
-        else if (digit_found)
-        {
-            break;
-        }
-
-        s++;
-    }
-    return (int)(result * sign);
+	while (*s != '\0')
+	{
+		if (*s == '-')
+			sign *= -1;
+		else if (*s == '+')
+			;
+		else if (*s >= '0' && *s <= '9')
+		{
+			digit_found = 1;
+			result = (result * 10) + (*s - '0');
+		}
+		else if (digit_found)
+			break;
+		s++;
+	}
+	return (int)(result * sign);
 }
