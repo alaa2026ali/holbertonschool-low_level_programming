6146814b4498f8ed29d3bd1c10e8b1308f614227
alaa2026ali <ali_alaa18@icloud.com>
2026-03-03 11:58:12 +0300

    Update 100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -11,15 +11,19 @@ int _atoi(char *s)
 	int sign = 1;
 	int num = 0;
 
-	 while (*s != '\0' && (*s < '0' || *s > '9'))
+
+	while (*s != '\0' && (*s < '0' || *s > '9'))
 	{
 		if (*s == '-')
 			sign *= -1;
 		else if (*s == '+')
-			; 
+		{
+			
+		}
 		s++;
 	}
 
+	
 	while (*s >= '0' && *s <= '9')
 	{
 		num = num * 10 + (*s - '0');
