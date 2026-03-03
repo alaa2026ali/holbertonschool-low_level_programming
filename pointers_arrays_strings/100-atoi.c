85f32d2b4767e03c8074997330c50d53f6eb8039
alaa2026ali <ali_alaa18@icloud.com>
2026-03-03 12:01:23 +0300

    Update 100-atoi.c

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,16 +1,16 @@
 #include "main.h"
+#include <limits.h> 
 
 /**
- * _atoi - converts a string to an integer
+ * _atoi - converts a string to an integer safely
  * @s: pointer to string
  *
- * Return: the integer value of the string
+ * Return: the integer value of the string, capped at INT_MAX/INT_MIN if overflow
  */
 int _atoi(char *s)
 {
 	int sign = 1;
 	int num = 0;
-
 
 	while (*s != '\0' && (*s < '0' || *s > '9'))
 	{
@@ -18,14 +18,17 @@ int _atoi(char *s)
 			sign *= -1;
 		else if (*s == '+')
 		{
-			
 		}
 		s++;
 	}
 
-	
 	while (*s >= '0' && *s <= '9')
 	{
+		if (sign == 1 && (num > (INT_MAX - (*s - '0')) / 10))
+			return (INT_MAX);
+		if (sign == -1 && (num > (-(INT_MIN + (*s - '0'))) / 10))
+			return (INT_MIN);
+
 		num = num * 10 + (*s - '0');
 		s++;
 	}
