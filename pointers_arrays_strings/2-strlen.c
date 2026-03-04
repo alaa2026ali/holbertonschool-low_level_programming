ea6486d7f096ee194c8b8aa4fb90e55f06db1720
alaa2026ali <ali_alaa18@icloud.com>
2026-03-04 10:28:01 +0300

    Update 2-strlen.c

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _strncpy - copies a string up to n characters
  * @dest: destination buffer
@@ -11,18 +10,15 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
-
 	while (i < n && src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
 	}
-
 	while (i < n)
 	{
 		dest[i] = '\0';
 		i++;
 	}
-
 	return (dest);
 }
