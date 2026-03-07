076354f386070fd0005e0f64c22c13163e876692
alaa2026ali <ali_alaa18@icloud.com>
2026-03-08 01:50:05 +0300

    Update 103-infinite_add.c

--- pointers_arrays_strings/103-infinite_add.c
+++ pointers_arrays_strings/103-infinite_add.c
@@ -2,45 +2,54 @@
 
 /**
  * infinite_add - adds two numbers stored as strings
- * @n1: first number
- * @n2: second number
- * @r: buffer to store result
+ * @n1: first number string
+ * @n2: second number string
+ * @r: buffer to store the result
  * @size_r: size of buffer
- * Return: pointer to result or 0 if result can't fit
+ *
+ * Return: pointer to result string or 0 if buffer too small
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i = 0, k, carry = 0, sum, len1 = 0, len2 = 0;
+    int len1 = 0, len2 = 0, i, j, k, sum, carry = 0;
 
-	while (n1[len1])
-		len1++;
+    /* find lengths of n1 and n2 */
+    while (n1[len1] != '\0')
+        len1++;
+    while (n2[len2] != '\0')
+        len2++;
 
-	while (n2[len2])
-		len2++;
+    /* check if buffer is big enough */
+    if (size_r <= (len1 > len2 ? len1 : len2))
+        return (0);
 
-	k = size_r - 1;
-	r[k] = '\0';
-	k--;
+    i = len1 - 1;
+    j = len2 - 1;
+    k = 0;
 
-	len1--;
-	len2--;
+    /* add digits from end */
+    while (i >= 0 || j >= 0 || carry)
+    {
+        sum = carry;
+        if (i >= 0)
+            sum += n1[i--] - '0';
+        if (j >= 0)
+            sum += n2[j--] - '0';
+        if (k >= size_r - 1) /* check buffer overflow */
+            return (0);
+        r[k++] = (sum % 10) + '0';
+        carry = sum / 10;
+    }
 
-	while (len1 >= 0 || len2 >= 0 || carry)
-	{
-		if (k < 0)
-			return (0);
+    r[k] = '\0';
 
-		sum = carry;
+    /* reverse the string */
+    for (i = 0, j = k - 1; i < j; i++, j--)
+    {
+        char temp = r[i];
+        r[i] = r[j];
+        r[j] = temp;
+    }
 
-		if (len1 >= 0)
-			sum += n1[len1--] - '0';
-
-		if (len2 >= 0)
-			sum += n2[len2--] - '0';
-
-		r[k--] = (sum % 10) + '0';
-		carry = sum / 10;
-	}
-
-	return (&r[k + 1]);
+    return (r);
 }
