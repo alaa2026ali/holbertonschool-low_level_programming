471bb95d16e4f020c16afc3a1f6b1fd0ba156bfc
alaa2026ali <ali_alaa18@icloud.com>
2026-03-07 22:25:50 +0300

    Update 101-print_number.c

--- pointers_arrays_strings/101-print_number.c
+++ pointers_arrays_strings/101-print_number.c
@@ -8,34 +8,35 @@
  */
 void print_number(int n)
 {
-    int divisor = 1;
+	int divisor = 1;
 
-    if (n == -2147483648)
-    {
-        _putchar('-');  
-        _putchar('2'); 
-        n = 147483648; 
-    }
+	if (n == -2147483648)
+	{
+		_putchar('-');
+		_putchar('2');
+		n = 147483648;
+	}
 
-    else if (n < 0)
-    {
-        _putchar('-');
-        n = -n; 
-    }
+	else if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
 
-    if (n == 0)
-    {
-        _putchar('0');
-        return;
-    }
+	/* handle zero */
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
 
-    while (n / divisor >= 10)
-        divisor *= 10;
+	while (n / divisor >= 10)
+		divisor *= 10;
 
-    while (divisor != 0)
-    {
-        _putchar('0' + n / divisor); 
-        n = n % divisor;          
-        divisor /= 10;           
-    }
+	while (divisor != 0)
+	{
+		_putchar('0' + n / divisor);
+		n = n % divisor;
+		divisor /= 10;
+	}
 }
