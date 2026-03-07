a536405dde73cfe5c3b93b323363ca8b577a735d
alaa2026ali <ali_alaa18@icloud.com>
2026-03-07 19:06:52 +0000

    Add 100-rot13.c

--- pointers_arrays_strings/100-rot13.c
+++ pointers_arrays_strings/100-rot13.c
@@ -8,17 +8,16 @@
  */
 char *rot13(char *s)
 {
-<<<<<<< HEAD
 	int i = 0, j;
 	char letters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char rot13letters[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	while (s[i] != '\0') 
+	while (s[i] != '\0')
 	{
 		j = 0;
-		while (letters[j] != '\0') 
+		while (letters[j] != '\0')
 		{
-			if (s[i] == letters[j]) 
+			if (s[i] == letters[j])
 			{
 				s[i] = rot13letters[j];
 				break;
@@ -28,25 +27,4 @@ char *rot13(char *s)
 		i++;
 	}
 	return (s);
-=======
-int i = 0, j;
-char letters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-char rot13letters[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-
-while (s[i] != '\0')
-{
-j = 0;
-while (letters[j] != '\0')
-{
-if (s[i] == letters[j])
-{
-s[i] = rot13letters[j];
-break;
-}
-j++;
-}
-i++;
-}
-return (s);
->>>>>>> ce27856 (Add 100-rot13.c)
 }
