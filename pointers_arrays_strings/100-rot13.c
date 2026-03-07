1cb87d95156da8f9e0b7f9876aac563d753fd614
alaa2026ali <ali_alaa18@icloud.com>
2026-03-07 18:36:00 +0000

    Add 100-rot13.c

---
+++ pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using ROT13
+ * @s: string to encode
+ *
+ * Return: pointer to encoded string
+ */
+char *rot13(char *s)
+{
+int i = 0, j;
+char letters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char rot13letters[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+while (s[i] != '\0')
+{
+j = 0;
+while (letters[j] != '\0')  
+		{
+if (s[i] == letters[j])  
+{
+s[i] = rot13letters[j];
+break;
+}
+j++;
+}
+i++;
+}
+return s;
+}
