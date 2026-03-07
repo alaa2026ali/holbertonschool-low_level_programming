30b07b6f3ef32dd78b251707620da91f79173da1
alaa2026ali <ali_alaa18@icloud.com>
2026-03-08 01:51:13 +0300

    Update 103-infinite_add.c

--- pointers_arrays_strings/103-infinite_add.c
+++ pointers_arrays_strings/103-infinite_add.c
@@ -11,45 +11,42 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-    int len1 = 0, len2 = 0, i, j, k, sum, carry = 0;
+	int len1 = 0, len2 = 0, i, j, k, sum, carry = 0;
 
-    /* find lengths of n1 and n2 */
-    while (n1[len1] != '\0')
-        len1++;
-    while (n2[len2] != '\0')
-        len2++;
+	while (n1[len1] != '\0')
+		len1++;
 
-    /* check if buffer is big enough */
-    if (size_r <= (len1 > len2 ? len1 : len2))
-        return (0);
+	while (n2[len2] != '\0')
+		len2++;
 
-    i = len1 - 1;
-    j = len2 - 1;
-    k = 0;
+	if (size_r <= (len1 > len2 ? len1 : len2))
+		return (0);
 
-    /* add digits from end */
-    while (i >= 0 || j >= 0 || carry)
-    {
-        sum = carry;
-        if (i >= 0)
-            sum += n1[i--] - '0';
-        if (j >= 0)
-            sum += n2[j--] - '0';
-        if (k >= size_r - 1) /* check buffer overflow */
-            return (0);
-        r[k++] = (sum % 10) + '0';
-        carry = sum / 10;
-    }
+	i = len1 - 1;
+	j = len2 - 1;
+	k = 0;
 
-    r[k] = '\0';
+	while (i >= 0 || j >= 0 || carry)
+	{
+		sum = carry;
+		if (i >= 0)
+			sum += n1[i--] - '0';
+		if (j >= 0)
+			sum += n2[j--] - '0';
+		if (k >= size_r - 1)
+			return (0);
+		r[k++] = (sum % 10) + '0';
+		carry = sum / 10;
+	}
 
-    /* reverse the string */
-    for (i = 0, j = k - 1; i < j; i++, j--)
-    {
-        char temp = r[i];
-        r[i] = r[j];
-        r[j] = temp;
-    }
+	r[k] = '\0';
 
-    return (r);
+	for (i = 0, j = k - 1; i < j; i++, j--)
+	{
+		char temp = r[i];
+		r[i] = r[j];
+		r[j] = temp;
+	}
+
+	return (r);
 }
