5aa364ef3bd6e5ab647d341b6142039ebdbb200b
alaa2026ali <ali_alaa18@icloud.com>
2026-03-08 11:23:19 +0300

    Update 3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -1,12 +1,15 @@
 #include "main.h"
 
 /**
- * _strspn - gets the length of a prefix substring
+ * _strspn - gets the length 
+ * of a prefix substring
  * @s: string to search
- * @accept: string containing accepted characters
+ * @accept: string 
+ * containing accepted characters
  *
- * Return: number of bytes in initial segment of s
- *         which consist only of bytes from accept
+ * Return: number of bytes 
+ * in initial segment of s
+ * which consist only of bytes from accept
  */
 unsigned int _strspn(char *s, char *accept)
 {
