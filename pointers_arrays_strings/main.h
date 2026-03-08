b72df99050dc7707a8d58e6feb6cf2bc092d06f7
alaa2026ali <ali_alaa18@icloud.com>
2026-03-08 11:16:10 +0300

    Update main.h

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stddef.h>
+
+#include <stddef.h> 
+
+/* Basic functions */
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -13,21 +16,25 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+
+/* String manipulation */
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
- char *string_toupper(char *);
+char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
- char *rot13(char *);
+char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
+
+/* Memory and pointer functions */
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
-int _strspn(char *s, char *accept);
 unsigned int _strspn(char *s, char *accept);
-#endif
+
+#endif 
