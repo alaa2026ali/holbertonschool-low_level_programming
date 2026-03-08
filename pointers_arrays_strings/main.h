4fdbf3252867f84e1e3386efaed94625bc162af5
alaa2026ali <ali_alaa18@icloud.com>
2026-03-08 11:03:08 +0300

    Update main.h

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -27,4 +27,5 @@ void print_buffer(char *b, int size);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
+int _strspn(char *s, char *accept);
 #endif
