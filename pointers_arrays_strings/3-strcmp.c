c03913ad7bc2347f8bc310aa3d7f121138e93e53
alaa2026ali <ali_alaa18@icloud.com>
2026-03-08 11:38:52 +0300

    Update 3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -10,35 +10,34 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-    unsigned int count = 0;
-    int found;
-    char *a;
+unsigned int count = 0;
+ int found;
+char *a;
 
-    while (*s != '\0')
-    {
-        found = 0;
-        a = accept;
+while (*s != '\0')
+{
+found = 0;
+a = accept;
 
-        while (*a != '\0')
-        {
-            if (*s == *a)
-            {
-                found = 1;
-                break;
-            }
-            a++;
-        }
+while (*a != '\0')
+{
+if (*s == *a)
+{
+found = 1;
+break;
+}
+a++;
+}
 
-        if (found)
-        {
-            count++;
-            s++;
-        }
-        else
-        {
-            break;
-        }
-    }
-
-    return count;
+if (found)
+{
+count++;
+s++;
+}
+else
+{
+break;
+}
+}
+return count;
 }
