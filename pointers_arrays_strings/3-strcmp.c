aabb97341387f40a8e704793511f8172270ccdb2
alaa2026ali <ali_alaa18@icloud.com>
2026-03-08 11:45:58 +0300

    Update 3-strcmp.c

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -5,7 +5,10 @@
  * @s: string to search
  * @accept: string containing accepted characters
  *
- * Return: Always 0
+ * Return: number of bytes in
+ *         the initial segment of s
+ *         which consist only of bytes
+ *         from accept
  */
 unsigned int _strspn(char *s, char *accept)
 {
