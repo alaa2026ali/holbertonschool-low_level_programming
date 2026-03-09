5c67de5def1388a9d0167ebadb0a8a22b303db60
alaa2026ali <ali_alaa18@icloud.com>
2026-03-09 12:46:27 +0300

    Update 5-sqrt_recursion.c

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -3,11 +3,11 @@
 /* helper function */
 int _sqrt_helper(int n, int i)
 {
-if (i * i > n)
-return (-1);
-if (i * i == n)
-return (i);
-return (_sqrt_helper(n, i + 1));
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (_sqrt_helper(n, i + 1));
 }
 
 /**
@@ -18,7 +18,7 @@ return (_sqrt_helper(n, i + 1));
  */
 int _sqrt_recursion(int n)
 {
-if (n < 0)
-eturn (-1);
-return (_sqrt_helper(n, 0));
+	if (n < 0)
+		return (-1);
+	return (_sqrt_helper(n, 0));
 }
