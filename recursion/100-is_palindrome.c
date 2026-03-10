8226c3201d7c63d84584c66ebed0d93dda377e39
alaa2026ali <ali_alaa18@icloud.com>
2026-03-10 10:21:11 +0300

    Update 100-is_palindrome.c

--- recursion/100-is_palindrome.c
+++ recursion/100-is_palindrome.c
@@ -8,24 +8,28 @@
  */
 int _strlen_recursion(char *s)
 {
-f (*s == '\0')
-retddddddddddddddg
- * @start: index ddof the first character
+    if (*s == '\0')
+        return (0);
+
+    return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * check_palindrome - checks characters from both ends of the string
+ * @s: the string
+ * @start: index of the first character
  * @end: index of the last character
- *dddddd
- * Returddddn: 1 if palindrome, 0 if not
- */dddddddd
+ *
+ * Return: 1 if palindrome, 0 if not
+ */
 int check_palindrome(char *s, int start, int end)
 {
-    /* If the start index crosses the end, it's a palindrome */
     if (start >= end)
         return (1);
 
-    /* If characters are not equal, it's not a palindrome */
     if (s[start] != s[end])
         return (0);
 
-    /* Check the next pair of characters */
     return (check_palindrome(s, start + 1, end - 1));
 }
 
@@ -39,13 +43,10 @@ int is_palindrome(char *s)
 {
     int len;
 
-    /* Get the length of the string */
     len = _strlen_recursion(s);
 
-    /* An empty string is a palindrome */
     if (len == 0)
         return (1);
 
-    /* Start checking from the first and last characters */
     return (check_palindrome(s, 0, len - 1));
 }
