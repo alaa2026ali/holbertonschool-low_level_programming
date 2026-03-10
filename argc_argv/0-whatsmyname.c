e079d417b81268750fe8d144230cb67751d068d6
alaa2026ali <ali_alaa18@icloud.com>
2026-03-10 08:15:52 +0000

    Add 0-whatsmyname.c

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the program
+ * @argc: Number of arguments (unused)
+ * @argv: Array of argument strings (argv[0] is program name)
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+(void)argc;
+printf("%s\n", argv[0])
+return 0;
+}
