b5e2b1e439fb7ecf879bc71dae613856cf527665
alaa2026ali <ali_alaa18@icloud.com>
2026-03-29 11:41:00 +0300

    Update 4-free_grid.c

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -6,15 +6,12 @@
  */
 void free_grid(int **grid, int height)
 {
-    int i;
-
-    if (grid == NULL)
-        return;
-
-    for (i = 0; i < height; i++)
-    {
-        free(grid[i]);
-    }
-
-    free(grid);
+int i;
+if (grid == NULL)
+return;
+for (i = 0; i < height; i++)
+{
+free(grid[i]);
+}
+free(grid);
 }
