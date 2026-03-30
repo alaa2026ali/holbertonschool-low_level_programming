605e2944b712700840b7546a222fb9c307ef1aa3
alaa2026ali <ali_alaa18@icloud.com>
2026-03-30 09:38:27 +0300

    Update main.h

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -5,4 +5,4 @@ int _putchar(char c);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
-#enfif
+#endif
