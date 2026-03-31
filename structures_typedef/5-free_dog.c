a705dd49aca4dc1c3098c8db723882197fabc159
alaa2026ali <ali_alaa18@icloud.com>
2026-03-31 11:43:43 +0300

    Update 5-free_dog.c

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,15 +1,16 @@
 #include <stdlib.h>
 #include "dog.h"
+
 /**
  * free_dog - frees a dog
  * @d: pointer to dog to free
  */
 void free_dog(dog_t *d)
 {
-  if (d) 
-{
-free(d->name);
-free(d->owner);
-free(d);
-}
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
