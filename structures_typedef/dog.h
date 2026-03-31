2e13dd6cc58187ded8d4b2d74684d91b4f9dd07e
alaa2026ali <ali_alaa18@icloud.com>
2026-03-31 10:20:11 +0300

    Add new dog structure and function prototypes

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,8 @@
 #ifndef DOG_H
 #define DOG_H
+
 #include <stddef.h>
+
 /**
  * struct dog - information about a dog
  * @name: the name of the dog
@@ -9,12 +11,15 @@
  */
 struct dog
 {
-char *name;
-float age;
-char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
+
 typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 dog_t new_dog(char *name, float age, char *owner);
 void free_dog(dog_t d);
+
 #endif
