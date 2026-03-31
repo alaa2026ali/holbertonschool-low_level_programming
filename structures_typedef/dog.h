67224f7212125a18314e5505ab318b974fde2da6
alaa2026ali <ali_alaa18@icloud.com>
2026-03-31 10:21:29 +0300

    Refactor dog.h to simplify dog structure initialization
    Removed unnecessary struct definition and updated init_dog function.

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,25 +1,19 @@
-#ifndef DOG_H
-#define DOG_H
-
+#include "dog.h"
 #include <stddef.h>
 
 /**
- * struct dog - information about a dog
- * @name: the name of the dog
- * @age: the age of the dog
- * @owner: the owner of the dog
+ * init_dog - initialize a variable of type struct dog
+ * @d: pointer to struct dog to initialize
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
  */
-struct dog
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	char *name;
-	float age;
-	char *owner;
-};
-
-typedef struct dog dog_t;
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-dog_t new_dog(char *name, float age, char *owner);
-void free_dog(dog_t d);
-
-#endif
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
