097e5fe7b79956b3d29f8370c4746400baf2ce62
alaa2026ali <ali_alaa18@icloud.com>
2026-03-31 10:12:34 +0300

    Update 1-init_dog.c

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -9,10 +9,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-    if (d != NULL) /* نتأكد أن المؤشر ليس فارغًا */
+    if (d != NULL)
     {
-        d->name = name;   /* نربط اسم الكلب */
-        d->age = age;     /* نربط العمر */
-        d->owner = owner; /* نربط صاحب الكلب */
+        d->name = name;
+        d->age = age;
+        d->owner = owner;
     }
 }
