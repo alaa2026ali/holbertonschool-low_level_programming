b9b4c4d2ea3c262e27d0e527f609dce85a3fd85c
alaa2026ali <ali_alaa18@icloud.com>
2026-03-31 11:18:34 +0300

    Update 4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>  /* malloc, free */
-#include <string.h>  /* strdup */
+#include <stdlib.h>
+#include <string.h>
 #include "dog.h"
-
 /**
  * new_dog - creates a new dog (allocates memory)
  * @name: name of the dog
@@ -13,42 +12,33 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *d;
-
-    /* allocate memory for the struct dog */
-    d = malloc(sizeof(dog_t));
-    if (d == NULL)
-        return (NULL);
-
-    /* copy name */
-    if (name != NULL)
-    {
-        d->name = strdup(name);  /* نسخ الاسم في مكان جديد */
-        if (d->name == NULL)     /* تحقق من malloc داخل strdup */
-        {
-            free(d);
-            return (NULL);
-        }
-    }
-    else
-        d->name = NULL;
-
-    /* copy owner */
-    if (owner != NULL)
-    {
-        d->owner = strdup(owner);  /* نسخ المالك في مكان جديد */
-        if (d->owner == NULL)
-        {
-            free(d->name);
-            free(d);
-            return (NULL);
-        }
-    }
-    else
-        d->owner = NULL;
-
-    /* نسخ العمر */
-    d->age = age;
-
-    return (d);
+dog_t *d;
+d = malloc(sizeof(dog_t));
+if (d == NULL)
+return (NULL);
+if (name != NULL)
+{
+d->name = strdup(name);
+if (d->name == NULL)
+{
+free(d);
+return (NULL);
+}
+}
+else
+d->name = NULL;
+if (owner != NULL)
+{
+d->owner = strdup(owner);
+if (d->owner == NULL)
+{
+free(d->name);
+free(d);
+return (NULL);
+}
+}
+else
+d->owner = NULL;
+d->age = age;
+return (d);
 }
