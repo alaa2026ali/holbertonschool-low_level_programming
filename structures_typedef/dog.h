9469bba6ebc38c80a86cfc13175cb6d8dea4dfbb
alaa2026ali <ali_alaa18@icloud.com>
2026-03-31 10:24:18 +0300

    Fix formatting and clean up dog.h header file

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,8 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
-
-#include <stddef.h>  /* لتعريف NULL */
-
+#include <stddef.h>
 /**
  * struct dog - information about a dog
  * @name: the name of the dog
@@ -15,11 +13,8 @@ char *name;
 float age;
 char *owner;
 };
-
 typedef struct dog dog_t;
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 dog_t new_dog(char *name, float age, char *owner);
 void free_dog(dog_t d);
-
 #endif
