11523e515011de92fae60d00e62b93c13f46a596
alaa2026ali <ali_alaa18@icloud.com>
2026-03-31 10:18:29 +0300

    Fix header guard and remove extra newlines

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,7 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
 #include <stddef.h>
-
 /**
  * struct dog - information about a dog
  * @name: the name of the dog
@@ -18,5 +17,4 @@ typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 dog_t new_dog(char *name, float age, char *owner);
 void free_dog(dog_t d);
-
 #endif
