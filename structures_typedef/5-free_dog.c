5c1b06dc2081633fbe290abbe9cf5a879bd7dbfd
alaa2026ali <ali_alaa18@icloud.com>
2026-03-31 11:39:27 +0300

    Update 5-free_dog.c

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,20 +1,16 @@
 #include <stdlib.h>
 #include "dog.h"
-
 /**
  * free_dog - frees a dog
  * @d: pointer to dog to free
  */
 void free_dog(dog_t *d)
 {
-    if (d == NULL)
-        return;          // إذا كان المؤشر NULL، لا نفعل شيء
-
-    if (d->name != NULL)
-        free(d->name);   // نحرر الذاكرة الخاصة بالاسم
-
-    if (d->owner != NULL)
-        free(d->owner);  // نحرر الذاكرة الخاصة بالمالك
-
-    free(d);             // نحرر الذاكرة الخاصة بالهيكل نفسه
+if (d == NULL)
+return;
+if (d->name != NULL)
+free(d->name);
+if (d->owner != NULL)
+free(d->owner);
+free(d);
 }
