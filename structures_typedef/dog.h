16de39640f154e19dd624e07a1be2411dce098dc
alaa2026ali <ali_alaa18@icloud.com>
2026-03-31 09:52:32 +0300

    Update dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - information about a dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ */
 struct dog
 {
 char *name;
