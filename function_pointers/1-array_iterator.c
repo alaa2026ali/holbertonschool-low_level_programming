252b4abfd00fc642bdc704421c94fc47db470e0a
alaa2026ali <ali_alaa18@icloud.com>
2026-04-05 10:31:48 +0300

    Update 1-array_iterator.c

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-
 /**
  * array_iterator - executes a function on each element of an array
  * @array: array of integers
@@ -8,13 +7,11 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-    unsigned int i;
-
-    if (array == NULL || action == NULL)
-        return;
-
-    for (i = 0; i < size; i++)
-    {
-        action(array[i]);
-    }
+unsigned int i;
+if (array == NULL || action == NULL)
+return;
+for (i = 0; i < size; i++)
+{
+action(array[i]);
+}
 }
