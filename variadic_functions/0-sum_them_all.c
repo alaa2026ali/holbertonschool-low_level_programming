296475340641579521ca4b2b9f510cd495dd292a
alaa2026ali <ali_alaa18@icloud.com>
2026-04-06 10:57:02 +0300

    Update 0-sum_them_all.c

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -15,12 +15,12 @@ int sum_them_all(const unsigned int n, ...)
     if (n == 0)
         return (0);
 
-    va_start(args, n);  // ابدأ قراءة المعاملات بعد n
+    va_start(args, n);
 
     for (i = 0; i < n; i++)
-        sum += va_arg(args, int);  // اجمع كل معامل
+        sum += va_arg(args, int);
 
-    va_end(args);  // تنظيف
+    va_end(args);
 
     return (sum);
 }
