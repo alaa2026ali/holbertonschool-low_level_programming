aea7d2747f8be0c7d9c1f34f98c5364fbca5240e
alaa2026ali <ali_alaa18@icloud.com>
2026-04-06 07:48:15 +0000

    Add 3-print_all.c

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - sums all its parameters
+ * @n: number of arguments
+ * Return: sum of all arguments, 0 if n == 0
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+    va_list args;
+    unsigned int i;
+    int sum = 0;
+
+    if (n == 0)
+        return (0);
+
+    va_start(args, n);  // ابدأ قراءة المعاملات بعد n
+
+    for (i = 0; i < n; i++)
+        sum += va_arg(args, int);  // اجمع كل معامل
+
+    va_end(args);  // تنظيف
+
+    return (sum);
+}
