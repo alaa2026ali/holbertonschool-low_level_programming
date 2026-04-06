b292020bc3b146fdfb7f3ba123bcb9a8642fe9b8
alaa2026ali <ali_alaa18@icloud.com>
2026-04-06 09:58:48 +0300

    Update 3-get_op_func.c

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,4 +1,5 @@
 #include "3-calc.h"
+
 /**
  * get_op_func - selects the correct function
  * @s: operator
@@ -6,20 +7,22 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
-op_t ops[] = {
-{"+", op_add},
-{"-", op_sub},
-{"*", op_mul},
-{"/", op_div},
-{"%", op_mod},
-{NULL, NULL}
-};
-int i = 0;
-while (ops[i].op != NULL)
-{
-if (*(ops[i].op) == *s && s[1] == '\0')
-return (ops[i].f);
-i++;
-}
-return (NULL);
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (ops[i].op != NULL)
+	{
+		if (*(ops[i].op) == *s && s[1] == '\0')
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
 }
