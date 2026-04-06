1f860db2186d84d493fef1b992c509030c5bd330
alaa2026ali <ali_alaa18@icloud.com>
2026-04-06 10:32:42 +0300

    Update variadic_functions.h

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,11 +1,12 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
-#include <stdarg.h>  // ضروري لاستخدام va_list
+#include <stdarg.h>
 
-/* بروتوتايب الدوال */
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+int _putchar(char c);
 
-/* هنا ممكن تضيف أي دوال Variadic أخرى في المشروع */
 
-#endif /* VARIADIC_FUNCTIONS_H */
+#endif
