c4db1dce1406609037c21ac839d2a8f5e7a1598e
alaa2026ali <ali_alaa18@icloud.com>
2026-04-06 11:36:57 +0300

    Update 1-print_numbers.c

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -1,7 +1,6 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include "variadic_functions.h"
-
 /**
  * print_numbers - prints numbers separated by a string
  * @separator: string to print between numbers
@@ -12,19 +11,15 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-    va_list args; 
-    unsigned int i;
-
-    va_start(args, n);
-
-    for (i = 0; i < n; i++)
-    {
-        printf("%d", va_arg(args, int));
-
-        if (separator != NULL && i != n - 1)
-            printf("%s", separator);
-    }
-
-    va_end(args);
-    printf("\n");
+va_list args; 
+unsigned int i;
+va_start(args, n);
+for (i = 0; i < n; i++)
+{
+printf("%d", va_arg(args, int));
+if (separator != NULL && i != n - 1)
+printf("%s", separator);
+}
+va_end(args);
+printf("\n");
 }
