f579161b24763cf21f8dfa0bb84e19883d119e60
alaa2026ali <ali_alaa18@icloud.com>
2026-04-07 10:28:35 +0300

    Update 3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,5 +1,5 @@
-#include <stdio.h>      // For printf
-#include <stdarg.h>     // For variadic functions (va_list, va_start, va_arg, va_end)
+#include <stdio.h>
+#include <stdarg.h>
 
 /**
  * print_all - prints any type of data based on a format string
@@ -17,47 +17,39 @@
  */
 void print_all(const char * const format, ...)
 {
-    va_list args;       // Variable to store the variadic arguments
-    int i = 0;          // Index for iterating through the format string
-    char *str;          // Temporary pointer to store string arguments
-    char *sep = "";     // Separator: empty for the first element
-
-    va_start(args, format);   // Initialize the va_list to retrieve arguments
-
-    // Loop through each character in the format string
-    while (format && format[i])
-    {
-        switch (format[i])
-        {
-            case 'c':   // Character
-                printf("%s%c", sep, va_arg(args, int)); // char promoted to int
-                sep = ", ";  // Update separator after first element
-                break;
-
-            case 'i':   // Integer
-                printf("%s%d", sep, va_arg(args, int));
-                sep = ", ";
-                break;
-
-            case 'f':   // Float
-                printf("%s%f", sep, va_arg(args, double)); // float promoted to double
-                sep = ", ";
-                break;
-
-            case 's':   // String
-                str = va_arg(args, char *);
-                if (!str)
-                    str = "(nil)";  // Replace NULL with (nil)
-                printf("%s%s", sep, str);
-                sep = ", ";
-                break;
-
-            default:    // Ignore unknown format characters
-                break;
-        }
-        i++; // Move to the next character in format
-    }
-
-    va_end(args);   // Clean up the va_list
-    printf("\n");   // Print newline at the end
+va_list args;
+int i = 0;
+char *str;
+char *sep = "";
+va_start(args, format);
+while (format && format[i])
+{
+switch (format[i])
+{
+case 'c':
+printf("%s%c", sep, va_arg(args, int));
+sep = ", ";
+break;
+case 'i':
+printf("%s%d", sep, va_arg(args, int));
+sep = ", ";
+break;
+case 'f':
+printf("%s%f", sep, va_arg(args, double));
+sep = ", ";
+break;
+case 's':
+str = va_arg(args, char *);
+if (!str)
+str = "(nil)";
+printf("%s%s", sep, str);
+sep = ", ";
+break;
+default:
+break;
+}
+i++;
+}
+va_end(args);
+printf("\n");
 }
