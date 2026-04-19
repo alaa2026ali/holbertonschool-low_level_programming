893b5b21d0b84314bbaa24e9711db2b777144600
alaa2026ali <ali_alaa18@icloud.com>
2026-04-19 11:13:17 +0300

    Change count variable to i for node counting

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -9,7 +9,7 @@
  */
 size_t print_list(const list_t *h)
 {
-size_t count = 0;
+size_t i = 0;
 while (h != NULL)
 {
 if (h->str == NULL)
@@ -17,7 +17,7 @@ printf("[0] (nil)\n");
 else
 printf("[%u] %s\n", h->len, h->str);
 h = h->next;
-count++;
+i++;
 }
-return (count);
+return (i);
 }
