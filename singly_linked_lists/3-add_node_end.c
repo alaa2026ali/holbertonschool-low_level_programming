b1f984828d89f565c201700da51ea44e30f256bc
alaa2026ali <ali_alaa18@icloud.com>
2026-04-20 08:08:17 +0000

    Add 3-add_node_end.c git commit -m Add

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,55 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_node_end - adds a new node at the end of a list
+ * @head: pointer to the head
+ * @str: string to add
+ *
+ * Return: new node or NULL
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node, *current;
+	unsigned int len = 0, i;
+
+	if (str == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	while (str[len])
+		len++;
+
+	new_node->str = malloc(len + 1);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	for (i = 0; i < len; i++)
+		new_node->str[i] = str[i];
+	new_node->str[len] = '\0';
+
+	new_node->len = len;
+	new_node->next = NULL;
+
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+
+	current = *head;
+
+	while (current->next != NULL)
+	current = current->next;
+
+curent->next = new_node;
+
+	return (new_node);
+}
