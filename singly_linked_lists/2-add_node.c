d90cd3ebb6053950abb7cb039abfc8b711cf2658
alaa2026ali <ali_alaa18@icloud.com>
2026-04-20 09:59:39 +0300

    Fix memory allocation and string assignment in add_node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -12,25 +12,32 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
 	unsigned int len = 0, i;
-	
+
 	if (str == NULL)
 		return (NULL);
+
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
+
 	while (str[len])
 		len++;
+
 	new_node->str = malloc(len + 1);
 	if (new_node->str == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
+
 	for (i = 0; i < len; i++)
-	new_node->str[i] = str[i];
+		new_node->str[i] = str[i];
 	new_node->str[len] = '\0';
+
 	new_node->len = len;
+
 	new_node->next = *head;
 	*head = new_node;
+
 	return (new_node);
 }
