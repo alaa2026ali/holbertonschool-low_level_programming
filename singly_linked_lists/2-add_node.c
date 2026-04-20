ef12817e90758ea89e8576a408573fea5b4497e5
alaa2026ali <ali_alaa18@icloud.com>
2026-04-20 09:48:33 +0300

    Refactor add_node function for clarity
    Removed unnecessary comments and cleaned up code formatting.

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -12,39 +12,24 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
 	unsigned int len = 0, i;
-
-	/* Check if input string is NULL */
 	if (str == NULL)
 		return (NULL);
-
-	/* Allocate memory for new node */
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
-
-	/* Calculate length of string */
 	while (str[len])
 		len++;
-
-	/* Allocate memory for string */
 	new_node->str = malloc(len + 1);
 	if (new_node->str == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
-
-	/* Copy string */
 	for (i = 0; i < len; i++)
-		new_node->str[i] = str[i];
+	new_node->str[i] = str[i];
 	new_node->str[len] = '\0';
-
-	/* Set length */
 	new_node->len = len;
-
-	/* Link new node to the beginning */
 	new_node->next = *head;
 	*head = new_node;
-
 	return (new_node);
 }
