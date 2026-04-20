e3ba1bdced7fe482ee03d1a7cd1fa2347076d094
alaa2026ali <ali_alaa18@icloud.com>
2026-04-20 08:28:28 +0000

    Add 4-free_list.c

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a linked list
+ * @head: pointer to the head
+ */
+void free_list(list_t *head)
+{
+	list_t *next_node;
+
+	while (head != NULL)
+	{
+		next_node = head->next;
+
+		free(head->str);
+		free(head);
+
+		head = next_node;
+	}
+}
