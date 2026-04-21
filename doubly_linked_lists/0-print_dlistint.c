6af66b3beb40f695be7fc4debd787ff975a8e3ad
alaa2026ali <ali_alaa18@icloud.com>
2026-04-21 05:49:31 +0000

    Add 0-print_dlistint.c

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all elements of a doubly linked list
+ * @h: pointer to the head
+ *
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		nodes++;
+	}
+
+	return (nodes);
+}
