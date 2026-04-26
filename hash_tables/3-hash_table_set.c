79fc50c4132ce34e96c10fd95f31fe1c0eeb2c48
alaa2026ali <ali_alaa18@icloud.com>
2026-04-26 11:22:47 +0300

    Update 3-hash_table_set.c

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -12,21 +12,27 @@
 hash_node_t *create_node(const char *key, const char *value)
 {
 	hash_node_t *node;
+
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
 		return (NULL);
+
 	node->key = strdup(key);
 	node->value = strdup(value);
+
 	if (node->key == NULL || node->value == NULL)
 	{
 		free(node->key);
-	free(node->value);
+		free(node->value);
 		free(node);
 		return (NULL);
 	}
+
 	node->next = NULL;
+
 	return (node);
 }
+
 /**
  * hash_table_set - adds or updates an element in the hash table
  * @ht: hash table
@@ -38,25 +44,33 @@ hash_node_t *create_node(const char *key, const char *value)
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *tmp, *new_node;
+	hash_node_t *tmp;
+	hash_node_t *new_node;
+
 	if (ht == NULL || key == NULL || *key == '\0')
-	return (0);
-index = key_index((const unsigned char *)key, ht->size);
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+
 	tmp = ht->array[index];
+
 	while (tmp)
 	{
-	if (strcmp(tmp->key, key) == 0)
-	{
-	free(tmp->value);
-    tmp->value = strdup(value);
-	return (1);
+		if (strcmp(tmp->key, key) == 0)
+		{
+			free(tmp->value);
+			tmp->value = strdup(value);
+			return (1);
+		}
+		tmp = tmp->next;
 	}
-	tmp = tmp->next;
-	}
+
 	new_node = create_node(key, value);
 	if (new_node == NULL)
-	return (0);
+		return (0);
+
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
+
 	return (1);
 }
