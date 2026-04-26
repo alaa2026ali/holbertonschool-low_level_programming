39b1815aa57f3f65999ec557858498cc6d07ef85
alaa2026ali <ali_alaa18@icloud.com>
2026-04-26 10:26:08 +0300

    Fix memory allocation for hash table creation

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -11,10 +11,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
 	unsigned long int i;
-
-	ht = malloc(sizeof(hash_table_t));
-	if (ht == NULL)
-		return (NULL);
+ht = malloc(sizeof(hash_table_t));
+if (ht == NULL)
+return (NULL);
 
 	ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (ht->array == NULL)
@@ -24,8 +23,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 
 	for (i = 0; i < size; i++)
-		ht->array[i] = NULL;
-
+	ht->array[i] = NULL;
 	ht->size = size;
 
 	return (ht);
