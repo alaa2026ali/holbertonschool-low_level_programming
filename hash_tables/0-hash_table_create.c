3ada1e7a3f82d16e69c356a15b85c7a10e65aa1e
alaa2026ali <ali_alaa18@icloud.com>
2026-04-26 10:26:46 +0300

    Update 0-hash_table_create.c

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -14,17 +14,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 ht = malloc(sizeof(hash_table_t));
 if (ht == NULL)
 return (NULL);
-
 	ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (ht->array == NULL)
 	{
-		free(ht);
-		return (NULL);
+	free(ht);
+	return (NULL);
 	}
-
 	for (i = 0; i < size; i++)
 	ht->array[i] = NULL;
 	ht->size = size;
-
 	return (ht);
 }
