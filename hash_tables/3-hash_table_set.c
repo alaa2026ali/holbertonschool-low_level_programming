6edd539e150c171cb1fc9eddcb15c3273e1e1c85
alaa2026ali <ali_alaa18@icloud.com>
2026-04-26 11:20:56 +0300

    Refactor hash_table_set function for clarity

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -3,55 +3,60 @@
 #include <string.h>
 
 /**
+ * create_node - creates a new hash node
+ * @key: key string
+ * @value: value string
+ *
+ * Return: pointer to new node, or NULL on failure
+ */
+hash_node_t *create_node(const char *key, const char *value)
+{
+	hash_node_t *node;
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (NULL);
+	node->key = strdup(key);
+	node->value = strdup(value);
+	if (node->key == NULL || node->value == NULL)
+	{
+		free(node->key);
+	free(node->value);
+		free(node);
+		return (NULL);
+	}
+	node->next = NULL;
+	return (node);
+}
+/**
  * hash_table_set - adds or updates an element in the hash table
  * @ht: hash table
  * @key: key string
  * @value: value string
  *
- * Return: 1 if success, 0 if fail
+ * Return: 1 on success, 0 on failure
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *new_node, *tmp;
-
+	hash_node_t *tmp, *new_node;
 	if (ht == NULL || key == NULL || *key == '\0')
-		return (0);
-
-	index = key_index((const unsigned char *)key, ht->size);
-
-	/* check if key already exists */
+	return (0);
+index = key_index((const unsigned char *)key, ht->size);
 	tmp = ht->array[index];
 	while (tmp)
 	{
-		if (strcmp(tmp->key, key) == 0)
-		{
-			free(tmp->value);
-			tmp->value = strdup(value);
-			return (1);
-		}
-		tmp = tmp->next;
-	}
-
-	/* create new node */
-	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
-		return (0);
-
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
-
-	if (new_node->key == NULL || new_node->value == NULL)
+	if (strcmp(tmp->key, key) == 0)
 	{
-		free(new_node->key);
-		free(new_node->value);
-		free(new_node);
-		return (0);
+	free(tmp->value);
+    tmp->value = strdup(value);
+	return (1);
 	}
-
-	/* insert at beginning (chaining) */
+	tmp = tmp->next;
+	}
+	new_node = create_node(key, value);
+	if (new_node == NULL)
+	return (0);
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
-
 	return (1);
 }
