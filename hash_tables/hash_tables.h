7ce7d29025680ffedfb2ad1258d1aaa4268011fc
alaa2026ali <ali_alaa18@icloud.com>
2026-04-26 10:30:08 +0300

    Add hash_djb2 function prototype to hash_tables.h

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -29,5 +29,6 @@ hash_node_t **array;
 
 /* Function prototype */
 hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
 
 #endif /* HASH_TABLES_H */
