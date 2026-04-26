7c45fbe973f5950cf69ef1b50b4da132b2a51cd5
alaa2026ali <ali_alaa18@icloud.com>
2026-04-26 10:27:29 +0300

    Refactor hash_table_create for better readability

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -7,21 +7,23 @@
  *
  * Return: pointer to the new hash table, or NULL on failure
  */
+
 hash_table_t *hash_table_create(unsigned long int size)
+
 {
-	hash_table_t *ht;
-	unsigned long int i;
+hash_table_t *ht;
+unsigned long int i;
 ht = malloc(sizeof(hash_table_t));
 if (ht == NULL)
 return (NULL);
-	ht->array = malloc(sizeof(hash_node_t *) * size);
-	if (ht->array == NULL)
-	{
-	free(ht);
-	return (NULL);
-	}
-	for (i = 0; i < size; i++)
-	ht->array[i] = NULL;
-	ht->size = size;
-	return (ht);
+ht->array = malloc(sizeof(hash_node_t *) * size);
+if (ht->array == NULL)
+{
+free(ht);
+return (NULL);
+}
+for (i = 0; i < size; i++)
+ht->array[i] = NULL;
+ht->size = size;
+return (ht);
 }
